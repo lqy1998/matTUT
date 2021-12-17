c1667855432c708f580154977bfcf356ccaec283
lqy1998 <68600731+lqy1998@users.noreply.github.com>
2021-12-17 17:35:09 +0800

    Delete spectral_analysis directory

--- spectral_analysis/extraction/get_mask.m
+++
@@ -1,9 +0,0 @@
-function [bw, band_ad, t, hist_smooth] = get_mask(effcube, b)
-
-band_ad = imadjust(effcube(:, :, b));
-[t, hist_smooth] = get_threshold(band_ad);
-bw = imbinarize(band_ad, t/(2^16));
-bw = bwareaopen(bw, 150, 8);    
-
-end
-
